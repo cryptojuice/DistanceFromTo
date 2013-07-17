c84cc488279ca7dc17d8f59e74cb785c10d294e4
Nick Robinson <grobins2@gmail.com>
2013-07-17 13:10:02 -0400

    Updated code formatting

--- distancebetween.py
+++ distancebetween.py
@@ -1,25 +1,46 @@
+"""
+This module uses the haversine forumla to find the distance between
+two locations
+
+Usage:
+
+python distancebetween "New York" "California"
+
+"""
+
 import sys
 import math
 from pygeocoder import Geocoder
 
-def haversine(lat1, lon1, lat2, lon2):
-	R = 6372.8
-	dLat = math.radians(lat2-lat1)
-	dLon = math.radians(lon2-lon1)
-	lat1 = math.radians(lat1)
-	lat2 = math.radians(lat2)
 
-	a = math.sin(dLat/2)**2 + math.sin(dLon/2)**2 * math.cos(lat1) * math.cos(lat2)
-	c = 2 * math.asin(math.sqrt(a))
-	return R * c
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Use haversine to find distance between two coordinates
+    """
+
+    earths_radius = 6372.8
+    dlat = math.radians(lat2-lat1)
+    dlon = math.radians(lon2-lon1)
+    lat1 = math.radians(lat1)
+    lat2 = math.radians(lat2)
+
+    a = math.sin(dlat/2)**2 + math.sin(dlon/2)**2 * math.cos(lat1)\
+            * math.cos(lat2)
+    c = 2 * math.asin(math.sqrt(a))
+    return earths_radius * c
+
 
 
 if __name__ == '__main__':
 
-  start = Geocoder.geocode(sys.argv[1])
-  end = Geocoder.geocode(sys.argv[2])
+    START = Geocoder.geocode(sys.argv[1])
+    END = Geocoder.geocode(sys.argv[2])
 
-  dkm = haversine(start.latitude, start.longitude, end.latitude, end.longitude)
-  dmi = haversine(start.latitude, start.longitude, end.latitude, end.longitude) * 0.62137
+    DKM = haversine(START.latitude, START.longitude, END.latitude,
+            END.longitude)
+    DMI = haversine(START.latitude, START.longitude, END.latitude, 
+            END.longitude) * 0.62137
 
-  print("Start Location: {0}\n End: location: {1}\n Distance: {2} km ({3} miles)".format(start.formatted_address, end.formatted_address,round(dkm),round(dmi)))
+    print("Start Location: {0}\n End: location: {1}\n Distance: {2} km ({3} mi\
+        les)".format(START.formatted_address, END.formatted_address
+            ,round(DKM) ,round(DMI)))
